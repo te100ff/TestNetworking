9f993f0374f6ac471ca0875ebc12e889f3767817
Stanislav <te100ff@inbox.ru>
2021-04-30 10:38:16 +0300

    Initial Commit

---
+++ TestNetworking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestNetworking
+//
+//  Created by Stanislav Testov on 30.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
